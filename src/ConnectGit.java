021ac530d9563ba8fb5ac114cee06bfba32a1251
LAPTOP-NV2K93JA\MYPC <youshj1004@itcommerce.co.kr>
2018-10-29 21:34:49 +0900

    Scanner 객체생성 입력받은 scanner 값 출력

--- src/ConnectGit.java
+++ src/ConnectGit.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class ConnectGit {
 
 	public static void main(String[] args) {
 		System.out.println("Connect git");
+		
+		Scanner sr = new Scanner(System.in);
+		String num = sr.nextLine();
+		System.out.printf("입력하신 숫자는 %s입니다.", num);
 	}
 
 }
