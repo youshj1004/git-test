f836ce41458f97198e46392610679122afaae0d8
Seong <tjdghkwnd1@naver.com>
2018-10-30 10:02:58 +0900

    git에서 파일 다운받고 소스 수정하고 다시 커밋하기

--- src/ConnectGit.java
+++ src/ConnectGit.java
@@ -8,6 +8,8 @@ public class ConnectGit {
 		Scanner sr = new Scanner(System.in);
 		String num = sr.nextLine();
 		System.out.printf("입력하신 숫자는 %s입니다.", num);  // 주석도 한글이 깨지나?
+		
+		System.out.println("재연결후 다시 커밋하기");
 	}
 
 }
